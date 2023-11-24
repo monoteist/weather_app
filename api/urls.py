137fe2f3ede765450c10989a8d9767dbc3b42890
i.ezerbaev <i.ezerbaev97@gmail.com>
2023-11-25 00:45:11 +0300

    add api app

---
+++ api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import include, path 
+from rest_framework import routers 
+
+app_name = 'api' 
+
+router_v1 = routers.DefaultRouter()
+
+urlpatterns = [ 
+    path('v1/', include(router_v1.urls)), 
+]
