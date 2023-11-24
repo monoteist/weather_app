4bdbc9bea5af150798b899a783e6a835edb243a3
i.ezerbaev <i.ezerbaev97@gmail.com>
2023-11-25 02:19:56 +0300

    City model create

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    name = models.CharField('Название города', max_length=50)
+    lat = models.FloatField('Широта')
+    lon = models.FloatField('Долгота')
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
